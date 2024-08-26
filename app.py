e8249d62a5299254a962fbee60102bf5bdf9e643
Eliud Duno <eliudduno@outlook.com>
2024-08-25 22:31:24 -0400

    añadido la lectura de datos y el objeto boton streamlit

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print('hola mundo')
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+car_data = pd.read_csv('./vehicles_us.csv')
+
+# Encabezado de la aplicacion con streamlit
+st.header('Encabezado inicial de la aplicacion')
+
+# creando un boton para iniciar la ejecución
+histo_inicio = st.button('Mostrar histograma')
+
+if histo_inicio:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crea un histograma
+    fig = px.histogram(car_data, x='odometer',
+                       title='Histograma de precios de los vehículos')
+    # mostrar un grafico pltoly interactivo
+    st.plotly_chart(fig, use_container_width=True)
